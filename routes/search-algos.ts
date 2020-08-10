1bc906fbc1c36a4e5a4fa3a744c95a5155db52fb
Khoa Nguyen <henrykhoanguyen@gmail.com>
2020-08-10 00:54:58 -0700

    Started client side

--- routes/search-algos.ts
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const {} = require("../controllers/search-algos.ts");
-
-const router = express.Router({ mergeParams: true }); //merge parameters together
-
-// router.route("/").get();
-
-module.exports = router;
--- server.js
+++ server.js
@@ -7,28 +7,16 @@ const colors = require("colors");
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
 
-// Route files
-const searches = require("./routes/search-algos.ts");
-const sorts = require("./routes/sort-algos.ts");
-
 const app = express();
 
 // Body parser
 app.use(express.json());
 app.use(cors());
 
-// Mount files
-app.use("/api/v1/search-algorithms", searches);
+app.use(express.static(__dirname + '/'));
 
 // Get port to be connected to
 const PORT = process.env.PORT || 5000;
-
-// Handle unhandled promise rejections
-process.on("unhandledRejection", (err, promise) => {
-  console.log(`Error: ${err.message}`.red);
-  // Close server and exit process
-  server.close(() => process.exit(1));
-});
 
 const server = app.listen(
   PORT,
@@ -36,3 +24,10 @@ const server = app.listen(
     `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
   )
 );
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // Close server and exit process
+  server.close(() => process.exit(1));
+});
